278e29066d52eab08edc86a60f103911cedc0690
shl <shl@tianqu.com.cn>
2019-04-24 14:42:41 +0800

    修复交互配置

--- meta.js
+++ meta.js
@@ -3,22 +3,46 @@
 * @title: 
 * @Date:   2019-04-23 16:07:22
 * @Last Modified by:   shl
-* @Last Modified time: 2019-04-24 10:08:07
+* @Last Modified time: 2019-04-24 14:41:35
 */
 module.exports = {
-  name: {
-    type: 'string',
-    default: 'mpd-project',
-    message: '项目名称',
+  init:{
+    name: {
+      type: 'string',
+      default: 'mpd-project',
+      message: '项目名称',
+    },
+    description: {
+      type: 'string',
+      message: '项目描述',
+      default: 'A mpd project',
+    },
+    author: {
+      type: 'string',
+      message: '作者信息',
+      default: "yourname"
+    }
   },
-  description: {
-    type: 'string',
-    message: '项目描述',
-    default: 'A mpd project',
-  },
-  author: {
-    type: 'string',
-    message: '作者信息',
-    default: "yourname"
+  mini:{
+    cssdir:{
+      type: 'string',
+      default: 'css',
+      message: '目标样式目录'
+    },
+    jsdir:{
+      type: 'string',
+      default: 'js',
+      message: '目标脚本目录'
+    },
+    cssout:{
+      type: 'string',
+      default: 'css',
+      message: '样式压缩导出目录'
+    },
+    jsout:{
+      type: 'string',
+      default: 'js',
+      message: '脚本压缩导出目录'
+    }
   }
 }
