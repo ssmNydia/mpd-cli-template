607f80eb0bf5aeacc856c86fb6ec9a51b2365736
shl <shl@tianqu.com.cn>
2019-12-10 16:10:46 +0800

    更新meta.js

--- meta.js
+++ meta.js
@@ -3,26 +3,66 @@
 * @title: 
 * @Date:   2019-04-23 16:07:22
 * @Last Modified by:   shl
-* @Last Modified time: 2019-04-24 14:41:35
+* @Last Modified time: 2019-12-10 16:10:27
 */
 module.exports = {
-  init:{
-    name: {
-      type: 'string',
-      default: 'mpd-project',
+  init: [
+    {
+      name: 'name',
+      type: 'input',
       message: '项目名称',
+      default: 'mpd-project'
     },
-    description: {
-      type: 'string',
+    {
+      name: 'description',
+      type: 'input',
       message: '项目描述',
       default: 'A mpd project',
     },
-    author: {
-      type: 'string',
+    {
+      name: 'author',
+      type: 'input',
       message: '作者信息',
       default: "yourname"
+    },
+    {
+      name: 'openHttps',
+      type: 'confirm',
+      message: '开发环境是否开启https',
+    },
+    {
+      name: 'openLint',
+      type: 'confirm',
+      message: '开发环境是否开启检测，推荐"No"',
+      default: false
+    },
+    {
+      name: 'clean',
+      type: 'confirm',
+      message: '生产环境打包前是否删除打包目录',
+    },
+    {
+      name: 'outHtml',
+      type: 'confirm',
+      message: '生产环境是否导出HTML文件',
+    },
+    {
+      name: 'openMinify',
+      type: 'confirm',
+      message: '生产环境是否启用HTML压缩',
+      when: function (answers) {
+        return answers.outHtml
+      }
+    },
+    {
+      name: 'favicon',
+      type: 'confirm',
+      message: '生成环境HTML页面是否设置favicon',
+      when: function (answers) {
+        return answers.outHtml
+      }
     }
-  },
+  ],
   mini:{
     cssdir:{
       type: 'string',
